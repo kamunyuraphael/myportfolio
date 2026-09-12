cbe244325c67b78cc6d759229422a522aa2feeef
kamunyuraphael <rahvickam@gmail.com>
2026-09-12 16:40:01 +0300

    Skills icon updates

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,6 +1,6 @@
 import type { IconType } from 'react-icons';
 import { SiMongodb, SiExpress, SiReact, SiNodedotjs, SiTypescript, SiPython } from 'react-icons/si';
-import { FaAws, FaShieldAlt, FaMicrochip, FaTasks, FaRobot, FaCogs } from 'react-icons/fa';
+import { FaAws, FaMicrochip, FaBrain, FaChartBar } from 'react-icons/fa';
 
 interface Skill {
     name: string;
@@ -17,6 +17,6 @@ export const skills: Skill[] = [
     { name: 'Python', icon: SiPython, category: 'Data & Tools' },
     { name: 'AWS Cloud', icon: FaAws, category: 'Data & Tools' },
     { name: 'AIoT', icon: FaMicrochip, category: 'Data & Tools' },
-    { name: 'Data Analysis', icon: FaCogs, category: 'Data & Tools' },
-    { name: 'Machine Learning', icon: FaRobot, category: 'Data & Tools' },
+    { name: 'Data Analysis', icon: FaChartBar, category: 'Data & Tools' },
+    { name: 'ML/AI', icon: FaBrain, category: 'Data & Tools' },
 ];
